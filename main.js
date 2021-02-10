26a3592f16c2da865e9ee6783437c5dd1d251ad4
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2021-02-10 20:23:31 +0700

    [build] index.html, main.js, images

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-    const app = {
+    const EGOIST = {
         data() {
             return {
-                msg: 'Hello, Vue3'
+            photogallery: [{ src:'./images/1.jpg', dis:'Extra terrestrial Biological Entities', done: false},
+                            {src:'./images/2.jpg', dis:'Greatest Hits 2011-2017 "Alter Ego"', done: false}]                                                                                                                                
+        }
+    },
+        methods: {
+            toggleDone(index){
+                this.photogallery[index].done = !this.photogallery[index].done
+            }
+        },
+        computed: {
+            countPhoto(){
+                return this.photogallery.length
             }
         }
 
     }
-    Vue.createApp(app).mount('#app')
+    Vue.createApp(EGOIST).mount('#EGOIST')
