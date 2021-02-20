1c4b005ecf4d95dd3d912856310834a8a5c928ad
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2021-02-20 14:31:38 +0700

    [update2] index.html

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
     const EGOIST = {
         data() {
             return {
-            search2:'',
-            photogallery: [{ src:'./images/1.jpg', dis:'Extra terrestrial Biological Entities', done: false},
-                            {src:'./images/2.jpg', dis:'Greatest Hits 2011-2017 "Alter Ego"', done: false}],            
-            search: [{ src:'./images/baseline_search_black_18dp.png', done: false}]                                                                                                                                                
             
-        }
+            photogallery: [{ src:'./images/1.jpg', dis:'Extra terrestrial Biological Entities', done: false, zoom: false},
+                            {src:'./images/2.jpg', dis:'Greatest Hits 2011-2017 "Alter Ego"', done: false, zoom: false}],                                                    
+            search: false,
+            filter: '',
+            notfound: false
+            
+            
+            
+         }
         
-    },
+        },
         methods: {
             toggleDone(index){
                 this.photogallery[index].done = !this.photogallery[index].done
             },
-            toggleDone2(index){
-                this.search[index].done = !this.search[index].done
+            toggleDone2(){
+                this.search = !this.search
+            },                       
+            toggleDone3(index){
+                this.photogallery[index].zoom = !this.photogallery[index].zoom
+            },
+            addtext(){
+                this.filter = this.search2
+                this.notfound = false
             }
         },
         computed: {
@@ -22,9 +33,16 @@
                 return this.photogallery.length
             },
             filteredList(){
-                return this.photogallery.filter(photogallery =>{
-                    return photogallery.dis.toLowerCase().includes(this.search2.toLowerCase())
-                  })
+                if (this.filter == '') {
+                    return this.photogallery
+                } else {
+                     ft = this.photogallery.filter ( d => d.dis.toLowerCase().includes(this.filter.toLowerCase()));
+                    if (ft.length == 0) {
+                        this.notfound = true;
+                    } else {
+                        return ft;
+                    }
+                }
             }
 
         }
